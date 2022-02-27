1ce2b27fd7055f2aaec5b726e4a8bf3507c25762
Anderson Santos <andersonsstr@hotmail.com>
2022-02-26 21:30:41 -0300

    Validações nos campos

--- public/assets/js.js
+++ public/assets/js.js
@@ -1,31 +1,35 @@
 $(document).ready(function(){  
-    var socket = io.connect("http://localhost:3000");
+   // var socket = io.connect("http://localhost:3000");
+	var socket = io.connect("http://40cf-201-17-210-168.ngrok.io/");
     var ready = false;
 
-    $("#submit").submit(function (e) {
-			e.preventDefault();
-			$("#nick").fadeOut();
-			$("#chat").fadeIn();
+    $("#submit").submit(function (e) {		
 			var name = $("#nickname").val();
-			var time = new Date();
-			$("#name").html(name);
-			$("#time").html('Primeiro login: ' + time.getHours() + ':' + time.getMinutes());
-
-			ready = true;
-			socket.emit("join", name);
-
+			if(name.length > 0){
+				e.preventDefault();
+				$("#nick").fadeOut();
+				$("#chat").fadeIn();
+				var time = new Date();
+				$("#name").html(name);
+				$("#time").html('Primeiro login: ' + time.getHours() + ':' + time.getMinutes());
+	
+				ready = true;
+				socket.emit("join", name);
+			}
 		});
 
 	$("#textarea").keypress(function(e){
         if(e.which == 13) {
         	var text = $("#textarea").val();
-        	$("#textarea").val('');
-        	var time = new Date();
-					$(".chat").append('<li class="self"><div class="msg"><span>' + $("#nickname").val() + ':</span><p>' + text + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
-					
-					socket.emit("send", text);
-					// automatically scroll down
-					document.getElementById('bottom').scrollIntoView();
+			if(text.length > 0){
+				$("#textarea").val('');
+				var time = new Date();
+				$(".chat").append('<li class="self"><div class="msg"><span>' + $("#nickname").val() + ':</span><p>' + text + '</p><time>' + time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+				
+				socket.emit("send", text);
+				// automatically scroll down
+				document.getElementById('bottom').scrollIntoView();				
+			}
         }
     });
 
